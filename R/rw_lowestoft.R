8d1e30d0366c31beffde34d1162a60f13fa5e6c5
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2015-09-10 09:49:44 +0000

    read_lowestoft activated

--- R/rw_lowestoft.R
+++ R/rw_lowestoft.R
@@ -4,6 +4,8 @@
 #'  \href{https://github.com/flr/FLCore/blob/master/R/io.VPAsuite.R}{FLCore::readVPAFile} function.
 #' The difference is that it is not dependent on the FLCore, including the S4-methods.
 #'
+#' @export
+#'
 #' @param file name of file, normally the index file name
 #' @param format output format, "matrix","wide","long"
 #' @param value.name Name of value, only relevant for format="long"
@@ -15,8 +17,9 @@ read_lowestoft <- function(file, format="matrix", value.name="x",sep = "", quiet
 
   if (!file.exists(file))
     {
-    if(quiet==TRUE) stop()
-    if(quiet!=TRUE) stop(paste("VPA index file", file, "does not exist"))
+    if(quiet==TRUE) message()
+    if(quiet!=TRUE) message(paste("VPA index file", file, "does not exist"))
+    return(NULL)
   }
 
   switch (as.character(file.access(file)),
@@ -103,3 +106,156 @@ read_lowestoft <- function(file, format="matrix", value.name="x",sep = "", quiet
   }
 }
 
+#' @title read_lowestoft2
+#'
+#' @description Modified \href{https://github.com/flr/FLCore/blob/master/R/io.VPAsuite.R}{FLCore::readVPA}
+#' function in the FLCore-package, except here it only returns a list or a data.frame
+#'
+#' @export
+#'
+#' @param file name of file, normally the index file name
+#' @param sep the separator, default is ""
+#' @param format character vector specifying output format. Default is "data.frame",
+#' but can also use "list" which returns a list of matrices.
+#' @param quiet boolean, default is TRUE
+
+read_lowestoft2 <- function(file, sep = "", format = "data.frame", quiet=TRUE) {
+  if (!file.exists(file)){
+    if(quiet!=TRUE) message(paste("VPA index file", file, "does not exist"))
+    return(NULL)
+  }
+  dir <- dirname(file)
+  files. <- scan(file, what = "character", skip = 2, sep = sep, quiet=quiet)
+  for(i in seq(length(files.)))
+    if (!grepl(dir,files.[i]))
+      files.[i] <- file.path(dir, files.[i], fsep = .Platform$file.sep)
+  range1 <- scan(files.[1], skip = 2, nlines = 1, sep = sep, quiet=quiet)
+  range2 <- scan(files.[1], skip = 3, nlines = 1, sep = sep, quiet=quiet)
+  range <- c(range1[1:2],range2[1:2])
+  ages <- range[3:4]
+  yrs <- range[1:2]
+  #FLStock. <- FLStock(catch.n=FLQuant(NA, dimnames = list(age = ages[1]:ages[2], year = yrs[1]:yrs[2], unit = "unique", season = "all", area = "unique")))
+  res <- list()
+  for (i in files.) {
+    if (!file.exists(i)){
+      if(quiet != TRUE) cat("File ", i, "does not exist", "\n")
+    }
+    if (file.exists(i)) {
+      a. <- read_lowestoft(i, sep=sep, quiet=quiet)
+      switch(as.character(scan(i, skip = 1, nlines = 1, sep = sep, comment.char='#', quiet=TRUE)[2]),
+             "1" = res$landings <-a.,
+             "2" = res$landings.n <-a.,
+             "3" = res$landings.wt <-a.,
+             "4" = res$stock.wt <-a.,
+             "5" = res$m <-a.,
+             "6" = res$mat <-a.,
+             "7" = res$harvest.spwn <-a.,
+             "8" = res$m.spwn <-a.,
+             "21"= res$discards <-a.,
+             "22"= res$discards.n <-a.,
+             "23"= res$discards.wt <-a.,
+             "24"= res$catch <-a.,
+             "25"= res$catch.n <-a.,
+             "26"= res$catch.wt <-a.,
+             "27"= res$harvest <-a.,
+             "28"= res$stock.n <-a. )
+    }
+  }
+
+  # change names to rvk-standard
+  # pending
+
+  res$range <- c(min = ages[1], max = ages[2],
+                 plusgroup = ages[2], minyear = yrs[1], maxyear = yrs[2])
+  res$desc <- paste("Imported from a VPA file (",
+                    file, "). ", date(), sep="")
+  res$name <- scan(file, nlines = 1, what = character(0),
+                   sep = "\n", quiet=TRUE)
+
+
+  # Landings
+  bya <- reshape2::melt(res$landings.n,value.name = "oL")
+  # Catch
+  if(is.null(res$catch.n)) {
+    bya$oC <- bya$oL
+  } else {
+    x <- reshape2::melt(res$catch.n,value.name="oC")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # Discards
+  if(is.null(res$discards.n)) {
+    bya$oD <- 0
+  } else {
+    x <- reshape2::melt(res$discards.n,value.name="oD")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # Landings weight
+  x <- reshape2::melt(res$landings.wt,value.name="lW")
+  bya <- plyr::join(bya,x,by=c("year","age"))
+  # Catch weigths
+  if(is.null(res$catch.wt)) {
+    bya$cW <- bya$lW
+  } else {
+    x <- reshape2::melt(res$catch.wt,value.name="cW")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # Discard weights
+  if(is.null(res$discards.wt)) {
+    bya$dW <- 0
+  } else {
+    x <- reshape2::melt(res$discards.wt,value.name="dW")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # SSB weights
+  if(is.null(res$stock.wt)) {
+    bya$ssbW <- bya$catch.wt
+  } else {
+    x <- reshape2::melt(res$stock.wt,value.name="ssbW")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # Maturity
+  if(is.null(res$mat)) {
+    bya$mat <- NA
+  } else {
+    x <- reshape2::melt(res$mat,value.name="mat")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # M
+  if(is.null(res$m)) {
+    bya$m <- NA
+  } else {
+    x <- reshape2::melt(res$m,value.name="m")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # pF
+  if(is.null(res$harvest.spwn)) {
+    bya$pF <- NA
+  } else {
+    x <- reshape2::melt(res$harvest.spwn,value.name="pF")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # pM
+  if(is.null(res$m.spwn)) {
+    bya$pM <- NA
+  } else {
+    x <- reshape2::melt(res$m.spwn,value.name="pM")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+  # fishing mortality
+  # Problem here is that res$harvest.spwn gets tested
+  #if(!is.null(res$harvest)) {
+  #  x <- reshape2::melt(res$harvest,value.name="f")
+  #  bya <- plyr::join(bya,x,by=c("year","age"))
+  #}
+  # stock in numbers
+  if(!is.null(res$stock.n)) {
+    x <- reshape2::melt(res$stock.n,value.name="n")
+    bya <- plyr::join(bya,x,by=c("year","age"))
+  }
+
+  if(format == "data.frame") {
+    res <- bya[order(bya$year),]
+  }
+
+  return(res)
+}
