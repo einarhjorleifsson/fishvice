5eb550605dfb74455022866305426a0e81fbad92
Einar Hjorleifsson <einar.hjorleifsson@gmail.com>
2016-03-30 18:18:33 +0000

    added flindices_to_rbya

--- R/converter.R
+++ R/converter.R
@@ -49,3 +49,30 @@ flstock_to_rbya <- function(x, scale=1, project = TRUE, plusgroup = TRUE)
   return(dplyr::as_data_frame(y))
 
 }
+
+#' @title Converts FLIndices object to rbya
+#'
+#' @description The rbya (results by year and age) is a long \code{data.frame}
+#' popular in Reykjavik
+#'
+#' @export
+#'
+#' @param x An FLIndices object
+#'
+flindices_to_rbya <- function(x) {
+
+  indices <- x@names
+  x <- x@.Data
+
+  x2 <- vector("list", length(indices))
+  names(x2) <- indices
+
+  for(i in seq_along(indices)) {
+    x2[[i]] <- reshape2::melt(d[[i]]@index)[,c("year","age","value")] %>%
+      dplyr::mutate(age = ifelse(age == "all",-9,age),
+                    sur = indices[i])
+  }
+
+  x <- x2 %>% purrr::map_df(as_data_frame)
+
+}
