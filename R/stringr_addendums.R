68b806e2b8d0a06523d7149ece4da5efc79c4dd7
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-04-23 13:46:43 +0000

    minor changes

--- R/stringr_addendums.R
+++ R/stringr_addendums.R
@@ -2,7 +2,6 @@
 #'
 #' @description Function strips all alphabetic characters (a-z and A-Z) from a string
 #'
-#' @export
 #'
 #' @param string A character vector
 
@@ -15,7 +14,6 @@ str_strip_text <- function(string) {
 #' @description Function strips all alphabetic and "special" characters from a string.
 #' Ergo, only digits 0-9 and the decimal (".") is returned.
 #'
-#' @export
 #'
 #' @param string A character vector
 #' @param out.numeric Boolean, if TRUE (default) string coerced to numeric.
@@ -31,7 +29,6 @@ str_strip_nondigits <- function(string, out.numeric = TRUE) {
 #' @description Function is inspired by str_trim function in the stringr package.
 #' The str_trim function is for trimming whitespace, here tabs are trimmed.
 #'
-#' @export
 #'
 #' @param string input character vector
 #' @param side side on which character string is removed (left, right or both)
@@ -50,7 +47,7 @@ str_trim_tab <- function(string,side='both') {
 #' @description Function is enspired by str_trim function in the stringr package.
 #' @param string input character vector
 #' @param side side on which character string is removed (left, right or both)
-#' @export
+#'
 
 str_trim_commas <- function(string,side='both') {
   #string <- stringr:::check_string(string)
